581115dfada5e64f26138ec77c15c6c90dd1aeb9
ChawTrexx <rupalchetia009@gmail.com>
2025-10-28 14:57:36 +0530

    Update and rename script.js to sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("chat-cache-v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./logo192.png",
+        "./logo512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
