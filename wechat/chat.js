0c38f3c4d24c234498bea0810ef9891788d72a17
ChawTrexx <rupalchetia009@gmail.com>
2026-01-30 13:57:02 +0530

    Update chat.js

--- wechat/chat.js
+++ wechat/chat.js
@@ -1,51 +1,30 @@
-import { auth, db } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { ref, push, onChildAdded, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { db } from "./firebase.js";
+import {
+  ref,
+  push,
+  onChildAdded
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
-let myNumber = null;
-let otherNumber = prompt("Enter number to chat");
+const uid = localStorage.getItem("uid");
+const username = localStorage.getItem("username");
 
-onAuthStateChanged(auth, async user => {
-  const snap = await (await fetch(
-    `https://wechat-54b07-default-rtdb.firebaseio.com/users/${user.uid}.json`
-  )).json();
+window.sendMessage = () => {
+  const msg = document.getElementById("msg").value;
+  if (!msg) return;
 
-  myNumber = snap.virtual_number;
-  document.getElementById("chatWith").innerText = otherNumber;
-  listenMessages();
-});
-
-function chatId() {
-  return [myNumber, otherNumber].sort().join("_");
-}
-
-window.sendMsg = () => {
-  const text = msg.value;
-  push(ref(db, "chats/" + chatId() + "/messages"), {
-    from: myNumber,
-    text,
+  push(ref(db, "messages"), {
+    uid,
+    username,
+    text: msg,
     time: Date.now()
   });
-  msg.value = "";
+
+  document.getElementById("msg").value = "";
 };
 
-function listenMessages() {
-  onChildAdded(ref(db, "chats/" + chatId() + "/messages"), snap => {
-    const d = snap.val();
-    const div = document.createElement("div");
-    div.className = "msg " + (d.from == myNumber ? "me" : "other");
-    div.innerText = d.text;
-    messages.appendChild(div);
-  });
-}
-
-window.startCall = (type) => {
-  const callId = Date.now();
-  set(ref(db, "calls/" + callId), {
-    caller: myNumber,
-    receiver: otherNumber,
-    type,
-    status: "calling"
-  });
-  alert(type.toUpperCase() + " call started");
-};
+onChildAdded(ref(db, "messages"), (snap) => {
+  const data = snap.val();
+  const div = document.createElement("div");
+  div.innerHTML = `<b>${data.username || "User"}:</b> ${data.text}`;
+  document.getElementById("messages").appendChild(div);
+});
